49831b34e24d073afa284fb642666753d5a3597d
andieGH <amgreenh@ucalgary.ca>
2019-02-10 16:50:49 -0700

    Added upper field

--- a2.js
+++ a2.js
@@ -5,11 +5,19 @@ function myFunction(id) {
 function clearScreen() {
   document.calc.result.value="";
 }
-
 function calculate() {
   try {
+  	var ans = document.calc.result.value;
+
+  	a = document.getElementById("answer");
+	a.innerText = ans;
+
+
+
+
     var input = eval(document.calc.result.value);
     document.calc.result.value=input;
+	
   } catch(err){
       document.calc.result.value="Error";
     }
