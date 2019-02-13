30c3d33e317b598f44ec82ad8edad894823388c6
andieGH <amgreenh@ucalgary.ca>
2019-02-12 20:04:58 -0700

    Refined

--- a2.js
+++ a2.js
@@ -1,18 +1,58 @@
+
+var str = "";
+var lastChar = "";
+
 function myFunction(id) {
-  document.calc.result.value+=id;
+
+	if (lastChar === "=") {
+		if ((id === "1") || (id === "2") || (id === "3") || (id === "4") || (id === "5") || (id === "6") || (id === "7") || (id === "8") || (id === "9")) {
+			str="";
+		}
+		else {
+			b = document.getElementById("answer");
+			b.innerText = "ANS= "+ str;
+		}
+	}
+	if (id === "=") {
+		try {
+			lastChar="="
+			var at = eval(str);
+			a = document.getElementById("screenText");
+			a.innerText = at;
+			b = document.getElementById("answer");
+			b.innerText = str;
+			str = at;
+
+		}
+		catch(err) {
+			a = document.getElementById("screenText");
+			a.innerText = "Error";
+		}
+	}
+
+	else {
+		str+=id;
+		a = document.getElementById("screenText");
+		a.innerText = str;
+		lastChar=id;
+	}
 }
 
+
 function clearScreen() {
-  document.calc.result.value="";
+	str = ""
+	a = document.getElementById("screenText");
+	a.innerText = str;
 }
 function calculate() {
   try {
+
+
   	var ans = document.calc.result.value;
+  	var anser = eval(ans);
 
   	a = document.getElementById("answer");
-	a.innerText = ans;
-
-
+	a.innerText = document.calc.result.value;
 
 
     var input = eval(document.calc.result.value);
